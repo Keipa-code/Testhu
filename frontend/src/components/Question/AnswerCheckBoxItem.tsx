999134b5f56174789343774da86e2ad637276d7b
Keipa-code <sikhed@gmail.com>
2021-10-08 16:56:13 +0500

    Some refactor on frontend

---
+++ frontend/src/components/Question/AnswerCheckBoxItem.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {Col, Form, FormGroup, Row} from "react-bootstrap";
+
+interface answerCheckBoxItemProps {
+    id: number;
+    correct: boolean;
+    text: string;
+}
+
+const AnswerCheckBoxItem = () => {
+    const [answerText, setAnswerText] = useState<string>('');
+    const [correctAnswers, setCorrectAnswers] = useState<[]>([]);
+
+    const changeAnswerText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswerText(e.target.value)
+    }
+    let checked = false
+
+    return (
+        <div>
+            <Form>
+                <Row className="align-items-center">
+                    <Col xs="auto">
+                        <Form.Control
+                            placeholder="Текст ответа"
+                            onChange={changeAnswerText}
+                        />
+                    </Col>
+
+                    <Col xs="auto">
+                        <Form.Check
+                            type="checkbox"
+                            checked={checked}
+                            className="mb-2"
+                            label="Правильный ответ"
+                        />
+                    </Col>
+
+                </Row>
+            </Form>
+
+        </div>
+    );
+};
+
+export default AnswerCheckBoxItem;
